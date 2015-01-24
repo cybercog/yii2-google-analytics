b454e461fdac769b733894f779a728bc1e17e195
a-komarev <ell@cybercog.su>
2015-01-25 01:23:11 +0300

    Comments update

--- src/widgets/GATracking.php
+++ src/widgets/GATracking.php
@@ -1,8 +1,18 @@
 <?php
+/**
+ * @link https://github.com/cybercog/yii2-twittable
+ * @copyright Copyright (c) 2015 LLC CyberCog
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
 namespace cybercog\yii\googleanalytics\widgets;
 
 use yii\base\Widget;
 
+/**
+ * Class GATracking
+ * @package cybercog\yii\googleanalytics\widgets
+ * @author Anton Komarev <ell@cybercog.su>
+ */
 class GATracking extends Widget
 {
     public $trackingId = null;
@@ -21,9 +31,8 @@ class GATracking extends Widget
         ];
     }
 
-
     public function run()
     {
         echo $this->render('tracking', $this->_viewParams);
     }
-} 
+}
--- src/widgets/views/tracking.php
+++ src/widgets/views/tracking.php
@@ -4,7 +4,6 @@
  * @var array   $trackingParams
  */
 ?>
-
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -13,5 +12,4 @@
 
     ga('create', '<?= $trackingId ?>', 'auto');
     ga('send', 'pageview');
-
 </script>
