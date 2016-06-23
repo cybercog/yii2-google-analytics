e78697094d7d1db6e750a7ae1d68d8d91d8c5129
Thiago Talma <thiagotalma@users.noreply.github.com>
2016-06-23 11:17:24 -0300

    Update tracking.php
    better format

--- src/widgets/views/tracking.php
+++ src/widgets/views/tracking.php
@@ -2,14 +2,19 @@
 /**
  * @var boolean $omitScriptTag
  * @var string  $trackingId
- * @var array   $trackingParams
- * @var array   $tackingPlugins
+ * @var array   $trackingConfig
+ * @var array   $fields
+ * @var array   $plugins
+ * @var string  $trackingFilename
+ * @var string  $trackingDebugTraceInit
  */
+
+if (!$omitScriptTag) {
+    echo "\n<script>";
+}
+echo $trackingDebugTraceInit;
 ?>
-<?php if (!$omitScriptTag) {
-    echo '<script>';
-} ?>
-    <?= $trackingDebugTraceInit ?>
+
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -17,12 +22,12 @@
 
     ga('create', '<?= $trackingId ?>', <?= $trackingConfig ?>);
     ga('send', 'pageview');
-    <?php foreach($fields as $field => $value) : ?>
+<?php foreach ($fields as $field => $value) : ?>
     ga('set', '<?= $field ?>', <?= $value ?>);
-    <?php endforeach ?>
-    <?php foreach($plugins as $plugin => $options) : ?>
+<?php endforeach ?>
+<?php foreach ($plugins as $plugin => $options) : ?>
     ga('require', '<?= $plugin ?>', <?= $options ?>);
-    <?php endforeach ?>
+<?php endforeach ?>
 <?php if (!$omitScriptTag) {
-    echo '</script>';
-} ?>
+    echo "</script>\n";
+}
